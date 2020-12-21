8d449bb21cd963dccef9db2fb9e3a96a619e1fc8
Paul <rekongstor@mail.ru>
2020-12-21 18:54:23 +0300

    Add project files.

---
+++ WebLab3/Client.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebLab3Client
+{
+    class HttpClient
+    {
+        static private System.Net.WebClient client;
+
+        static void Send1(String data, String method)
+        {
+            client.UploadData("http://localhost:8080/", method, Encoding.UTF8.GetBytes(data));
+        }
+
+        static void Send(String postData, String method)
+        {
+            var data = Encoding.ASCII.GetBytes(postData);
+
+
+            WebRequest request = WebRequest.Create("http://localhost:8080/");
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            request.Method = method;
+            request.ContentLength = data.Length;
+
+            if (data.Length > 0)
+                request.GetRequestStream().Write(data, 0, data.Length);
+
+            // Get the response.
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse) request.GetResponse();
+                // Display the status.
+                Console.WriteLine("Status: " + response.StatusDescription);
+                // Get the stream containing content returned by the server.
+                Stream dataStream = response.GetResponseStream();
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = reader.ReadToEnd();
+                // Display the content.
+                Console.WriteLine(responseFromServer);
+                // Cleanup the streams and the response.
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+            }
+            catch (System.Net.WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch
+            {
+                Console.WriteLine("Unhandled exception");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+
+            client = new WebClient();
+
+            // Add a user agent header in case the
+            // requested URI contains a query.
+
+            client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+
+            while (true)
+            {
+                String line = Console.ReadLine();
+                if (line == null)
+                    continue;
+
+                int space = line.IndexOf(' ');
+                String request;
+                if (space == -1)
+                    request = line.ToUpper();
+                else
+                    request = line.Substring(0, space).ToUpper();
+
+                switch (request)
+                {
+                    case "PUT":
+                        Send(line.Substring(space + 1), "PUT");
+                        break;
+                    case "POST":
+                        Send(line.Substring(space + 1), "POST");
+                        break;
+                    case "GET":
+                        Send("", "GET");
+                        break;
+                    case "PATCH":
+                        Send(line.Substring(space + 1), "PATCH");
+                        break;
+                    case "QUIT":
+                        return;
+                }
+            }
+        }
+    }
+}
---
+++ WebLab3Server/Server.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace WebLab3Server
+{
+    class Data
+    {
+        public string name { get; set; }
+        public string value { get; set; }
+    }
+
+    class HttpServer
+    {
+        public static HttpListener listener;
+        public static string url = "http://localhost:8080/";
+        public static int pageViews = 0;
+        public static int requestCount = 0;
+        //static private SortedList<string, string> map;
+        static private Data dataServer;
+
+
+        public static async Task HandleIncomingConnections()
+        {
+            bool runServer = true;
+
+            // While a user hasn't visited the `shutdown` url, keep on handling requests
+            while (runServer)
+            {
+                // Will wait here until we hear from a connection
+                HttpListenerContext ctx = await listener.GetContextAsync();
+
+                // Peel out the requests and response objects
+                HttpListenerRequest req = ctx.Request;
+                HttpListenerResponse resp = ctx.Response;
+
+                String reqData = "";
+
+                if (req.HasEntityBody)
+                {
+                    using (System.IO.Stream body = req.InputStream) // here we have data
+                    {
+                        using (System.IO.StreamReader reader = new System.IO.StreamReader(body, req.ContentEncoding))
+                        {
+                            reqData = reader.ReadToEnd();
+                        }
+                    }
+                }
+
+                // Print out some info about the request
+                Console.WriteLine("Request #: {0}", ++requestCount);
+                Console.WriteLine(req.Url.ToString());
+                Console.WriteLine(req.HttpMethod);
+                Console.WriteLine(req.UserHostName);
+                Console.WriteLine(req.UserAgent);
+                Console.WriteLine("BODY: " + reqData);
+                Console.WriteLine();
+
+                string jsResp = "";
+                Data jss;
+                // If `shutdown` url requested w/ POST, then shutdown the server after serving the page
+                switch (req.HttpMethod)
+                {
+                    case "POST":
+                        try
+                        {
+                            jss = JsonSerializer.Deserialize<Data>(reqData, new JsonSerializerOptions());
+
+                            if (dataServer == null)
+                                dataServer = new Data();
+
+                            if (jss.name != null)
+                                dataServer.name = jss.name;
+                            if (jss.value != null)
+                                dataServer.value = jss.value;
+                            resp.StatusCode = 200;
+                        }
+                        catch
+                        {
+                            resp.StatusCode = 400;
+                        }
+                        break;
+                    case "GET":
+                        jsResp = JsonSerializer.Serialize<Data>(dataServer);
+                        resp.StatusCode = 200;
+                        break;
+                    case "PUT":
+                        if (dataServer == null)
+                        {
+                            resp.StatusCode = 400;
+                            break;
+                        }
+                        try
+                        {
+                            jss = JsonSerializer.Deserialize<Data>(reqData, new JsonSerializerOptions());
+                            if (jss.name != null)
+                                dataServer.name = jss.name;
+                            if (jss.value != null)
+                                dataServer.value = jss.value;
+                            resp.StatusCode = 200;
+                        }
+                        catch
+                        {
+                            resp.StatusCode = 400;
+                        }
+                        break;
+                    case "PATCH":
+                        try
+                        {
+                            jss = JsonSerializer.Deserialize<Data>(reqData, new JsonSerializerOptions());
+                            if (jss.name != null)
+                                dataServer.name = jss.name;
+                            if (jss.value != null)
+                                dataServer.value = jss.value;
+                            resp.StatusCode = 200;
+                        }
+                        catch
+                        {
+                            resp.StatusCode = 400;
+                        }
+                        break;
+                }
+
+                // Make sure we don't increment the page views counter if `favicon.ico` is requested
+                if (req.Url.AbsolutePath != "/favicon.ico")
+                    pageViews += 1;
+
+                // Write the response info
+                string disableSubmit = !runServer ? "disabled" : "";
+                byte[] data = Encoding.UTF8.GetBytes(jsResp);
+                resp.ContentType = "text/text";
+                resp.ContentEncoding = Encoding.UTF8;
+                resp.ContentLength64 = data.LongLength;
+
+                // Write out to the response stream (asynchronously), then close it
+                await resp.OutputStream.WriteAsync(data, 0, data.Length);
+                resp.Close();
+            }
+        }
+
+
+        public static void Main(string[] args)
+        {
+            //map = new SortedList<string, string>();
+            dataServer = null;
+            // Create a Http server and start listening for incoming connections
+            listener = new HttpListener();
+            listener.Prefixes.Add(url);
+            listener.Start();
+            Console.WriteLine("Listening for connections on {0}", url);
+
+            // Handle requests
+            Task listenTask = HandleIncomingConnections();
+            listenTask.GetAwaiter().GetResult();
+
+            // Close the listener
+            listener.Close();
+        }
+    }
+}
